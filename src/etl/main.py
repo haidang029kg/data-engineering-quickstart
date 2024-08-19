9b437767cb7176b9a904bf4773e1641778f7cf88
Dang Vu <vnhd1995@gmail.com>
2024-08-19 13:35:08 +0700

    update etl

--- src/etl/main.py
+++ src/etl/main.py
@@ -1,40 +1,62 @@
+import glob
+
 import numpy as np
 import pandas as pd
 import xarray as xr
 
-DIR_NAME = "nc_files"
-FILE_NAMES = [
-    # "MERRA2_100.inst3_3d_asm_Np.19800101.nc4",
-    #
-    "MERRA2_400.inst3_3d_asm_Np.20240101.nc4",
-    "MERRA2_400.inst3_3d_asm_Np.20240102.nc4",
-    # "MERRA2_400.inst3_3d_asm_Np.20240103.nc4",
-    # "MERRA2_400.inst3_3d_asm_Np.20240104.nc4",
-]
+NC_DIR_PATH = "/home/vnhd/nc_files"
+FILES = glob.glob(f"{NC_DIR_PATH}/*.nc4")
 
 
 def main():
-    print("Hello world")
-    files = [f"./{DIR_NAME}/{f_name}" for f_name in FILE_NAMES]
-
-    res = extract(files[0])
+    res = extract(FILES[0])
     res = transform(res)
+    load(res)
 
 
 def extract(nc_file_path: str) -> xr.Dataset:
+    print("==========extract data")
     return xr.open_dataset(nc_file_path)
 
 
 def transform(ds: xr.Dataset) -> pd.DataFrame:
-    print(ds)
+    print("==========transform data")
     d_humidity: xr.DataArray = ds.QV
     d_air_temp: xr.DataArray = ds.T
 
-    print(d_air_temp)
+    # humidity transform
+    df_humidity = d_humidity.to_dataframe(name="value")
+    df_humidity = df_humidity.rename(columns={"value": "humidity"})
+    df_humidity["humidity_unit"] = "kg kg-1"
+    df_humidity = df_humidity.reset_index()
+
+    print(df_humidity)
+
+    # air temperature transform
+    df_air_temp = d_air_temp.to_dataframe(name="value")
+    df_air_temp = df_air_temp.rename(columns={"value": "air_temp"})
+    df_air_temp["air_temp_unit"] = "K"
+    df_air_temp = df_air_temp.reset_index()
+
+    print(df_air_temp)
+
+    try:
+        # merge
+        merged_df = pd.merge(
+            df_humidity,
+            df_air_temp,
+            on=["time", "lev", "lat", "lon"],
+        )
+        return merged_df
+
+    except Exception as exc:
+        print(exc)
+        raise exc
 
 
 def load(data: pd.DataFrame):
-    pass
+    print("==========load data")
+    print(data.head())
 
 
 if __name__ == "__main__":
